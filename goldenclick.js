ac997683ba930a12527cd9a330cdff4a0330269b
Kirc <iva.degtarev@gmail.com>
2016-03-18 19:52:21 +0300

    Update goldenclick.js

--- goldenclick.js
+++ goldenclick.js
@@ -62,6 +62,6 @@ var defaultOptions = {
 
 function func()
 {
-var timerId = setInterval(simulate,20000,document.getElementById("goldenCookie"),"click")
+var timerId = setInterval(simulate,1000,document.getElementById("goldenCookie"),"click")
 }
 func();
